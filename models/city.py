abff1c2bffecc47b4e29601299c423e562979a08
Omewah <joelomewah@ymail.com>
2023-08-13 15:06:12 +0100

    add AirBnb models

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class City: subclass of BaseModel"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """The Class City"""
+
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Review module: subclass of BaseModel"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """The Review model"""
+    place_id = ""
+    user_id = ""
+    text = ""
